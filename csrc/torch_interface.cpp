f5747adcb73b253cc44008631a96c3973f99c6c4
SzymonOzog <szymon.ozog@gmail.com>
2025-10-07 09:04:08 +0000

    Copy kernels from sgl branch; setup package

---
+++ csrc/torch_interface.cpp
@@ -0,0 +1,136 @@
+#include <pybind11/functional.h>
+#include <torch/python.h>
+#include <torch/all.h>
+#include <ATen/cuda/CUDAContext.h>
+#include <cuda.h>
+#include <cuda_fp8.h>
+#include <cuda_bf16.h>
+
+#define MOE_ARGS const __nv_fp8_e4m3* x,\
+        const float* x_scale,\
+        const __nv_fp8_e4m3* w,\
+        const float* w_scale,\
+        __nv_bfloat16* out,\
+        const int* sorted_token_ids,\
+        const int* expert_ids,\
+        const int* num_tokens_post_padded,\
+        const int top_k,\
+        int M,\
+        int K,\
+        int N,\
+        int sorted_num
+
+#define MOE_CALL static_cast<__nv_fp8_e4m3*>(x.data_ptr()), \
+            static_cast<float*>(x_scale.data_ptr()), \
+            static_cast<__nv_fp8_e4m3*>(w.data_ptr()), \
+            static_cast<float*>(w_scale.data_ptr()), \
+            static_cast<__nv_bfloat16*>(out.data_ptr()), \
+            static_cast<int*>(sorted_token_ids.data_ptr()), \
+            static_cast<int*>(expert_ids.data_ptr()), \
+            static_cast<int*>(num_tokens_post_padded.data_ptr()), \
+            top_k, \
+            x.size(0), \
+            x.size(1), \
+            w.size(1), \
+            sorted_token_ids.size(0)
+
+void fused_moe_w8a8(MOE_ARGS);
+void fused_moe_w8a8_regtiling(MOE_ARGS);
+void fused_moe_w8a8_prefetching(MOE_ARGS);
+void fused_moe_w8a8_smem(MOE_ARGS);
+void fused_moe_w8a8_db(MOE_ARGS);
+void fused_moe_w8a8_tb(MOE_ARGS);
+void fused_moe_w8a8_mb(MOE_ARGS);
+void fused_moe_w8a8_sacc(MOE_ARGS);
+void fused_moe_w8a8_pc(MOE_ARGS);
+void fused_moe_w8a8_ast(MOE_ARGS);
+void fused_moe_w8a8_wgmma(MOE_ARGS, int block_m);
+void fused_moe_w8a8_wgmma_tma(const __nv_fp8_e4m3* x,
+        const float* x_scale,
+        __nv_fp8_e4m3* w,
+        const float* w_scale,
+        __nv_bfloat16* out,
+        const int* sorted_token_ids,
+        const int* expert_ids,
+        const int* num_tokens_post_padded,
+        const int top_k,
+        int M, int K, int N,
+        int sorted_num, int block_m);
+void fused_moe_w8a8_wgmma_swiglu(MOE_ARGS, int block_m);
+void fused_moe_w8a8_wgmma_tma_swiglu(const __nv_fp8_e4m3* x,
+        const float* x_scale,
+        __nv_fp8_e4m3* w,
+        const float* w_scale,
+        __nv_bfloat16* out,
+        const int* sorted_token_ids,
+        const int* expert_ids,
+        const int* num_tokens_post_padded,
+        const int top_k,
+        int M, int K, int N,
+        int sorted_num, int block_m);
+
+torch::Tensor fused_moe_launcher(
+        torch::Tensor& x,
+        torch::Tensor& x_scale,
+        torch::Tensor& w,
+        torch::Tensor& w_scale,
+        torch::Tensor& sorted_token_ids,
+        torch::Tensor& expert_ids,
+        torch::Tensor& num_tokens_post_padded,
+        int top_k,
+        int kernel_variant,
+        int BM
+        )
+{
+    auto options = torch::TensorOptions().dtype(at::ScalarType::BFloat16).device(w.device());
+    torch::Tensor out = torch::empty({x.size(0) * top_k, w.size(1)}, options);
+    switch (kernel_variant)
+    {
+        case 0:
+            fused_moe_w8a8(MOE_CALL);
+            break;
+        case 1:
+            fused_moe_w8a8_prefetching(MOE_CALL);
+            break;
+        case 2:
+            fused_moe_w8a8_smem(MOE_CALL);
+            break;
+        case 3:
+            fused_moe_w8a8_db(MOE_CALL);
+            break;
+        case 4:
+            fused_moe_w8a8_tb(MOE_CALL);
+            break;
+        case 5:
+            fused_moe_w8a8_mb(MOE_CALL);
+            break;
+        case 6:
+            fused_moe_w8a8_sacc(MOE_CALL);
+            break;
+        case 7:
+            fused_moe_w8a8_pc(MOE_CALL);
+            break;
+        case 8:
+            fused_moe_w8a8_ast(MOE_CALL);
+            break;
+        case 9:
+            fused_moe_w8a8_wgmma(MOE_CALL, BM);
+            break;
+        case 10:
+            fused_moe_w8a8_wgmma_tma(MOE_CALL, BM);
+            break;
+        case 11:
+            out = torch::empty({x.size(0) * top_k, w.size(1)/2}, options);
+            fused_moe_w8a8_wgmma_swiglu(MOE_CALL, BM);
+            break;
+        case 12:
+            out = torch::empty({x.size(0) * top_k, w.size(1)/2}, options);
+            fused_moe_w8a8_wgmma_tma_swiglu(MOE_CALL, BM);
+            break;
+    }
+    return out;
+}
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("fused_moe_w8a8", &fused_moe_launcher);
+}
---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+import setuptools
+import importlib
+import importlib.resources
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import torch
+torch.utils.cpp_extension.COMMON_NVCC_FLAGS = []
+
+
+if __name__ == '__main__':
+
+    sources = ['./csrc/torch_interface.cpp',
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_prefetching.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_smem.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_db.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_tb.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_mb.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_sacc.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_pc.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_ast.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_wgmma.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_wgmma_tma.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_wgmma_swiglu.cu",
+               "./csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_wgmma_tma_swiglu.cu",
+               ]
+
+    setuptools.setup(
+        name='alpha-kernel-python',
+        version='0.0.1',
+        ext_modules=[
+            CUDAExtension(
+                name='alpha_kernel',
+                sources=sources,
+            )
+        ],
+        cmdclass={
+            'build_ext': BuildExtension
+        }
+    )
+
