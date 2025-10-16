69c78ef8de2a81e7e4874234cca0bc315241ec83
SzymonOzog <szymon.ozog@gmail.com>
2025-10-16 15:46:15 +0000

    Simplify scaling

--- csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_up_down_ast.cu
+++ csrc/kernels/fused_moe_w8a8/fused_moe_w8a8_up_down_ast.cu
@@ -933,12 +933,15 @@ __global__ __launch_bounds__(WN*32 + PRODUCER_THREADS) void fused_moe_w8a8_wgmma
             }
         }
 
-        float scale_x[TPT];
         for(int t = 0; t < TPT; t++)
         {
             if (token_dest[t] < M * top_k)
             {
-                scale_x[t] = topk_weights[token_dest[t]];
+                const float topk_w = topk_weights[token_dest[t]];
+                for(int tn = 0; tn<TN; tn++)
+                {
+                    token_max[tn][t/2][t%2] *= topk_w;
+                }
             }
         }
 
@@ -995,7 +998,7 @@ __global__ __launch_bounds__(WN*32 + PRODUCER_THREADS) void fused_moe_w8a8_wgmma
                                 acc += token_max[tn][tm][s]*tile_acc[tn2 + t/4][tn][tm][t%4];
                             }
                         }
-                        tile[t] = acc*scale_w[tn2/2]*scale_x[tm*2 + t%2];
+                        tile[t] = acc*scale_w[tn2/2];
                     }
                     int out_row = tm * 8 + lane_id%8;
                     int out_col = warp_id*16 + ((lane_id/8)*8)%16 + tn2*64 + (lane_id/16)*64;
